d8d7a7f9f82b34a0f8053be8d0d4aca19fa19ced
王雷 <wanglei@chinacache.com>
2013-05-21 17:23:49 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peigenyuan::Application.config.secret_token = '221caf141c99d9304b6fcd912ca259e23cc6eff30a6c515a4cfa15849069efb7b0a563f003a564ba03492db93e52287d334a69590b913215ed46572d8f630bf7'
